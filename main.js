3c6815f8ae71c2d93f058cc4949976841fcd0b97
Jonas <jonas.schiller@mailbox.org>
2016-04-13 15:57:11 +0200

    make username match case insensitive

--- main.js
+++ main.js
@@ -78,7 +78,8 @@ function sendMessage(text, user) {
     if (m) {
         text = text.replace('@' + m[1], '').trim().replace(/\s\s/g, ' ');
         for (u in users) {
-            if (users[u] == m[1]) {
+            var re = new RegExp(m[1],'i');
+            if (users[u].match(re)) {
                 count++;
                 if (text.match(/\.(jpg|png|jpeg|bmp)$/i) && fs.existsSync(text)) {
                     adapter.log.debug('Send photo to "' + m[1] + '": ' + text);
