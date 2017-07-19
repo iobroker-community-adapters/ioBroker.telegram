ccb45394c601508da19c858cda1f0edba0dfc926
Haba1234 <b_roman@inbox.ru>
2017-07-19 09:59:27 +0300

    Bag fix
    There was an incorrect order of writing variables

--- main.js
+++ main.js
@@ -624,13 +624,13 @@ function connect() {
         bot.on('callback_query', function (msg) {
         // write received answer into variable
             adapter.log.debug('callback_query: ' + JSON.stringify(msg));
-            adapter.setState('communicate.request', '[' + msg.from.first_name + ']' + msg.data, function (err) {
-                if (err) adapter.log.error(err);
-            });
             callbackQueryId = msg.id;
             adapter.setState('communicate.requestMessageId', msg.message.message_id, function (err) {
                 if (err) adapter.log.error(err);
             });
+            adapter.setState('communicate.request', '[' + msg.from.first_name + ']' + msg.data, function (err) {
+                if (err) adapter.log.error(err);
+            });    
         });
     }
 }
