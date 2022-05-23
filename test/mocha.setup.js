d78ce0245633aef965942ab84400ea425bafcaad
Matthias Kleine <github.com9@emailu.de>
2022-05-23 14:10:49 +0200

    Added eslint, removed gulp, updated tests, added adapter-dev

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -1,1 +1,14 @@
-process.on("unhandledRejection", (r) => { throw r; });
+// Don't silently swallow unhandled rejections
+process.on('unhandledRejection', (e) => {
+	throw e;
+});
+
+// enable the should interface with sinon
+// and load chai-as-promised and sinon-chai by default
+const sinonChai = require('sinon-chai');
+const chaiAsPromised = require('chai-as-promised');
+const { should, use } = require('chai');
+
+should();
+use(sinonChai);
+use(chaiAsPromised);
