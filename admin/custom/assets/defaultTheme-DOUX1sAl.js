e2913ee21e4a6cfd2faf614fa2bee4606c929911
GermanBluefox <dogafox@gmail.com>
2025-03-11 09:40:49 +0000

    Fixing load of vite

---
+++ admin/custom/assets/defaultTheme-DOUX1sAl.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createTheme-BeniWjyR.js";const a=e();export{a as d};
---
+++ admin/custom/assets/jsx-runtime-C7g9_nkN.js
@@ -0,0 +1,9 @@
+import{C as l}from"./ConfigCustomTelegramSet__loadShare__react__loadShare__-qT6HkOsG.js";var p={exports:{}},_={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var a=l,i=Symbol.for("react.element"),m=Symbol.for("react.fragment"),y=Object.prototype.hasOwnProperty,d=a.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,x={key:!0,ref:!0,__self:!0,__source:!0};function u(t,r,s){var e,o={},n=null,f=null;s!==void 0&&(n=""+s),r.key!==void 0&&(n=""+r.key),r.ref!==void 0&&(f=r.ref);for(e in r)y.call(r,e)&&!x.hasOwnProperty(e)&&(o[e]=r[e]);if(t&&t.defaultProps)for(e in r=t.defaultProps,r)o[e]===void 0&&(o[e]=r[e]);return{$$typeof:i,type:t,key:n,ref:f,props:o,_owner:d.current}}_.Fragment=m;_.jsx=u;_.jsxs=u;p.exports=_;var c=p.exports;export{c as j};
