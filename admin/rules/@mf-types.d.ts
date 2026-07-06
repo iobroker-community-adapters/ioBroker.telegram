a66ecc34ae1ed38be219c8c80e425355cd962447
GermanBluefox <dogafox@gmail.com>
2026-07-06 12:40:30 +0200

    Removed some garbage

--- admin/rules/@mf-types.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'REMOTE_ALIAS_IDENTIFIER/ActionTelegram';
-    type PackageType<T> = T extends 'REMOTE_ALIAS_IDENTIFIER/ActionTelegram' ? typeof import('REMOTE_ALIAS_IDENTIFIER/ActionTelegram') :any;
