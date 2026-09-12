a7a6d2410812451b5e2a3faa86d75b769e0ca0a1
GermanBluefox <dogafox@gmail.com>
2026-09-12 20:43:57 +0300

    chore: release v6.0.1
    - (@GermanBluefox) Updated packages and improved the rules block
    - (@patricknitsch) Retried getUpdates 409 conflicts quickly before falling back to a slow restartart

--- admin/custom/assets/mf-entry-bootstrap-0-df2bf7ab.js
+++ admin/custom/assets/mf-entry-bootstrap-0-df2bf7ab.js
@@ -37,11 +37,11 @@ const __mfImport = (src) =>
 
 
 (async () => {
-  const __mfHostInit = await __mfImport("./hostInit-C4h1aULA.js");
+  const __mfHostInit = await __mfImport("./hostInit-CuMnfnDC.js");
   await __mfHostInit.__tla;
   const { initHost } = __mfHostInit;
   await initHost();
-  const __mfPendingShares = await __mfImport("./pendingShares-BxKfdzMz.js").catch(() => undefined);
+  const __mfPendingShares = await __mfImport("./pendingShares-Cxf76vil.js").catch(() => undefined);
   if (__mfPendingShares && typeof __mfPendingShares.preloadPendingShares === "function") await __mfPendingShares.preloadPendingShares();
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
@@ -50,4 +50,4 @@ const __mfImport = (src) =>
   if (__mfReactServerModuleCache?.pendingShareLoads) {
     await Promise.all(__mfReactServerModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-BZiL4b3_.js"));
+})().then(() => __mfImport("./index-DvK7dHLG.js"));
--- admin/custom/customComponents.js
+++ admin/custom/customComponents.js
@@ -1,7 +1,7 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__ConfigCustomTelegramSet__customComponents_js-sVoLZ8ST.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__ConfigCustomTelegramSet__customComponents_js-CVo5tXt9.js";export{t as get,e as init};
 if (typeof document !== 'undefined' && document.head) {
   try {
-    for (const __mfWarmupPath of ["assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__ConfigCustomTelegramSet__customComponents_js-sVoLZ8ST.js","assets/virtualExposes-Ditkhfhv.js","assets/virtual_mf-exposes___mfe_internal__ConfigCustomTelegramSet__customComponents_js-YXqBAHsP.js","assets/_virtual_mf-localSharedImportMap___mfe_internal__ConfigCustomTelegramSet__mf_owner__1-DKrXS3o8.js","assets/vite-preload-helper-CaC3yEL3.js","assets/dist-D4uJqRBO.js"]) {
+    for (const __mfWarmupPath of ["assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__ConfigCustomTelegramSet__customComponents_js-CVo5tXt9.js","assets/virtualExposes-Ditkhfhv.js","assets/virtual_mf-exposes___mfe_internal__ConfigCustomTelegramSet__customComponents_js-YXqBAHsP.js","assets/_virtual_mf-localSharedImportMap___mfe_internal__ConfigCustomTelegramSet__mf_owner__1-B79-UQNs.js","assets/vite-preload-helper-CaC3yEL3.js","assets/dist-D4uJqRBO.js"]) {
       const __mfWarmupLink = document.createElement('link');
       __mfWarmupLink.rel = 'modulepreload';
       __mfWarmupLink.crossOrigin = '';
